dbc8a6a39e9308429a4cfc42016620fcc6df9bfd
Zhengyao Gu <41970486+zhengyaogu@users.noreply.github.com>
2026-02-25 21:58:25 -0600

    Remove sitemap integration from Astro config
    Removed sitemap integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhengyaogu.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), tailwind()]
 });
 // sitemap()
