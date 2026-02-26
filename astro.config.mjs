23d6d78199e1bef89f13314f49e0ff5226b12b92
Zhengyao Gu <41970486+zhengyaogu@users.noreply.github.com>
2026-02-25 21:25:28 -0600

    Remove base path from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhengyaogu.github.io',
-  base: '/zhengyaogu.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
